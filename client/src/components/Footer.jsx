fe11b30a2123f1798f293d844730b675c5b2eb66
nimesh-liyanage <nimesh0723@gmail.com>
2024-04-16 17:25:45 +0530

    IPFU Management

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2024 All Rights Reserved By ITP24_B5_03
+    </p>
+  </div>
+);
+
+export default Footer;
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as IPFUDashboard } from './IPFUDashboard';
+export { default as Dashboard } from './Dashboard';
+export { default as SuspendedProducts } from './SuspendedProducts';
+export { default as DonationPrograms } from './DonationPrograms';
+export { default as Shops } from './Shops';
+export { default as Donors } from './Donors';
+export { default as ProductReceivers } from './ProductReceivers';
